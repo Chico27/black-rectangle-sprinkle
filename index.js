dd395ba7fa164776ec07f1dc0ea4e22ec5559fab
chico27 <masamasa.987654321@gmail.com>
2019-09-07 15:14:25 +0900

    new

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const line = 20;
 const column = 20;
-// const maxSeats = 40;
+const maxRectangle = line*column;
 var limitCt = 0;
 window.onload = function() {
   setInterval("sprinkle()", 1000);
 
-  selectChangeCt = Math.floor(Math.random()*10)+1
-
   let k = 1;
   for (let i = 1; i <= line; i++) {
     for (let j = 1; j <= column; j++) {
+      selectChangeCt = Math.floor(Math.random()*10)+1
       const div = document.createElement('div');
       div.id='black_rectangele_' + k;
       div.classList.add('box');
       div.classList.add('flex');
-      div.classList.add('change_ct_' + selectChangeCt);
+      // div.classList.add('change_ct_' + selectChangeCt);
       document.body.appendChild(div);
       k++;
     }
@@ -25,37 +24,17 @@ window.onload = function() {
 
 //現在時刻を表示する関数
 function sprinkle(){
-  console.log(Math.floor(Math.random()*10)+1);
-  let attr = document.getElementById("black_rectangele_1");
-  if (!attr.classList.contains('box_disappear')) {
-    attr.classList.add("box_disappear");
-  } else {
-    attr.classList.remove("box_disappear");
+  ct = 1;
+  while(ct <= maxRectangle){
+    randomNum = Math.random();
+    if (randomNum < 0.5){
+      let attr = document.getElementById("black_rectangele_" + ct);
+      if (!attr.classList.contains('box_disappear')) {
+        attr.classList.add("box_disappear");
+      } else {
+        attr.classList.remove("box_disappear");
+      }
+    }
+    ct++;
   }
 }
-
-var setSheat = function(clickedButton) {
-  let setCt = true;
-  while(setCt  && limitCt < maxSeats){
-    let seatCt = Math.floor(Math.random() * 40 + 1);
-    let attr = document.getElementById("seat_" + seatCt);
-    if (!attr.classList.contains('man') && !attr.classList.contains('woman')){
-      attr.classList.add(clickedButton);
-      setCt = false;
-      limitCt++;
-    }
-  }
-};
-
-var reset = function() {
-  for (let k = 1; k <= maxSeats; k++) {
-      var attr = document.getElementById("seat_" + k);
-      if (attr.classList.contains('man')){
-        attr.classList.remove('man');
-      }
-      if (attr.classList.contains('woman')){
-        attr.classList.remove('woman');
-      }
-  }
-  limitCt = 0;
-};
